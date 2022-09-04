c1f8b3df6fff43c6955c889b3aef14a542c015cc
Aleksey Knigin <alexyo2626@gmail.com>
2022-09-04 18:22:32 +0300

    add task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const cardMurkup = createGalleryMurkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardMurkup);
+
+//рендер разметки
+function createGalleryMurkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`;
+    })
+    .join('');
+}
+
+// подключение галереии
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
